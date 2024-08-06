1ef9da503ac03df80a4dce5378f46681d16dd577
deepak04iiitn <dky422003@gmail.com>
2024-08-07 00:40:07 +0530

    implemented loading effect and pull to refresh

--- app/businessList/[category].js
+++ app/businessList/[category].js
@@ -1,6 +1,6 @@
 // its a dynamic route , which means that category name can change anytime => businessList/category
 
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams, useNavigation } from 'expo-router'
 import { collection, query , where, getDocs } from 'firebase/firestore';
@@ -11,6 +11,7 @@ import { Colors } from '../../constants/Colors';
 export default function BusinessListByCategory() {
 
     const [businessList , setBusinessList] = useState([]);
+    const [loading , setLoading] = useState(false);
 
     const navigation = useNavigation();
     const {category} = useLocalSearchParams();             // the {} should be same as the category name in the file []
@@ -30,6 +31,8 @@ export default function BusinessListByCategory() {
 
     const getBusinessList = async() => {
 
+        setLoading(true);
+
         const q = query(collection(db , 'BusinessList') , where("category" , '==' , category));
 
         const querySnapshot = await getDocs(q);
@@ -37,19 +40,27 @@ export default function BusinessListByCategory() {
         querySnapshot.forEach((doc) => {
             setBusinessList(prev => [...prev , doc.data()]);
         })
+
+        setLoading(false);
+
     }
 
 
   return (
     <View>
 
-      {businessList?.length > 0 ? 
+      {businessList?.length > 0 && loading === false ? 
         <FlatList
             data={businessList}
+            onRefresh={getBusinessList}                   // pull to refresh
+            refreshing={loading}
             renderItem={({item , index}) => (
                 <BusinessListCard business={item} key={index} />
             )} 
         /> :
+            loading ? <ActivityIndicator size={'large'} color={Colors.PRIMARY} style={{
+                marginTop:'60%'
+            }} /> :
             <Text style={{
               fontSize:20,
               fontFamily:'outfit-bold',

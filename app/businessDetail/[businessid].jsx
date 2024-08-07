5c58ac8ca01f059fd10d366256447f0c5944663b
deepak04iiitn <dky422003@gmail.com>
2024-08-08 00:48:28 +0530

    business detail page

---
+++ app/businessDetail/[businessid].jsx
@@ -0,0 +1,63 @@
+import { View, Text, ActivityIndicator, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { collection, getDoc, query, where } from 'firebase/firestore';
+import { db } from '../../configs/FirebaseConfig';
+import { Colors } from '../../constants/Colors';
+import Intro from '../../components/BusinessDetail/Intro';
+import ActionButton from '../../components/BusinessDetail/ActionButton';
+import About from '../../components/BusinessDetail/About';
+
+
+export default function BusinessDetail() {
+
+    const [business , setBusiness] = useState();
+    const [loading , setLoading] = useState(false);
+
+    const {businessid} = useLocalSearchParams();
+
+    useEffect(() => {
+        GetBusinessDetailById();
+    } , [])
+
+    // used to get business detail by id
+    const GetBusinessDetailById = async() => {
+
+        setLoading(true);
+
+        const docRef = doc(db , 'BusinessList' , businessid);
+        const docSnap = await getDoc(docRef);
+
+        if(docSnap.exists())
+        {
+            setBusiness(docSnap.data());
+            setLoading(false);
+        }
+        else
+        {
+            console.log("No such document!");
+            setLoading(false);
+        }
+    }
+
+  return (
+
+    <ScrollView>
+        
+        {loading ? 
+            <ActivityIndicator size={'large'} color={Colors.PRIMARY} style={{marginTop:'70%'}} /> :
+            <View>
+                {/* Intro */}
+                <Intro business={business} />
+
+                {/* Action Buttons */}
+                <ActionButton business={business} />
+
+                {/* About Section */}
+                <About business={business} />
+            </View>
+        }
+      
+    </ScrollView>
+  )
+}
